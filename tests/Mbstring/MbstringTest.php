06d12cd2d1fca299f441220a0f6c674597ead2e9
Gordon P. Hemsley <gphemsley@gphemsley.org>
2026-05-25 00:54:33 -0400

    Fix error handlers for Mbstring and Php83 tests on PHP 7

--- tests/Mbstring/MbstringTest.php
+++ tests/Mbstring/MbstringTest.php
@@ -676,7 +676,9 @@ class MbstringTest extends TestCase
         $this->expectExceptionMessage($expectedError);
 
         set_error_handler(static function ($errno, $errstr, $errfile, $errline) {
-            throw new \ErrorException($errstr, $errno, $errfile, $errline);
+            if (\E_USER_WARNING === $errno) {
+                throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+            }
         });
 
         try {
@@ -864,7 +866,9 @@ class MbstringTest extends TestCase
         $this->expectExceptionMessage('mb_trim(): Argument #3 ($encoding) must be a valid encoding, "NULL" given');
 
         set_error_handler(static function ($errno, $errstr, $errfile, $errline) {
-            throw new \ErrorException($errstr, $errno, $errfile, $errline);
+            if (\E_USER_WARNING === $errno) {
+                throw new \ErrorException($errstr, 0, $errno, $errfile, $errline);
+            }
         });
 
         try {
